efaff42672753481b7bef6201ce0cbdf91ec8eeb
StanislavPaladin <PaladinStanis@gmail.com>
2020-08-11 20:41:27 +0300

    feat: completed task for lesson_04

--- main.js
+++ main.js
@@ -1,48 +1,17 @@
 'use strict'
-
-// Решение через многомерный массив:
-
-let days = [];
-days['ru'] = ['Пн, Вт, Ср, Чт, Пт, Сб, Вс'];
-days['en'] = ['Sunday, Monday, Tuesday, Wednesday, Thursday, Saturday'];
-let language = prompt ('Введите ваш язык. Ru / En');
-console.log(days[language]);
-
-
-// let x = prompt('ru / eng');
-
-//Решение через if
- 
-
-// if (x == 'ru') {
-//     console.log('Пн, Вт, Ср, Чт, Пт, Сб, Вс');
-
-// } else if (x == 'en') {
-//     console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Saturday');
-// }
-// else 
-// {
-//     alert('Вы ввели неверное значение! Попробуйте ещё раз.')
-// }
-
-    
-// Switch-case:
-
-// switch(x) {
-//     case 'ru':
-//         console.log('Пн, Вт, Ср, Чт, Пт, Сб, Вс');
-//         break;
-//     case 'en':
-//         console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Saturday');
-//         break;
-// };
-
-
-// Вторая задача:
-let namePerson = prompt('Имя пользователя');
-
-let input = (namePerson == 'Артём') ? 'Директор' :
-  (namePerson == 'Максим') ? 'Преподаватель' :
-  (namePerson) ? 'Студент' :
-  '';
-console.log( input );
+let b;
+function cutFunction(a) {
+    a = prompt();
+    b = a.split(' ').join('');
+    if (typeof a != 'string') {
+        alert('Введите строку');
+        return;
+    }
+    else if (b.length >= 30) {
+        console.log((b.slice(0, 30)) + '...');
+    } 
+    else if (b.length < 30) {
+        console.log(b);
+    }
+}
+cutFunction();

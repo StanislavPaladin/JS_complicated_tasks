4446a6e65d86c2a22dc1ab045400f2ba64d4fc3a
StanislavPaladin <PaladinStanis@gmail.com>
2020-08-17 21:20:27 +0300

    feat: completed complicated task in lesson_07

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
 'use strict';
 
-let array = ['244', '27', '11223', '4123123', '4123123123', '7811', '123']; 
 
-array.forEach((item) => {
-  if (item.startsWith('2') || item.startsWith('4')) {
-    console.log(item);
-  }
-});
+let week = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+  today = new Date().getDay() - 1;
 
-
-
-
-let n = 1000;
-nextPrime:
-for (let i = 2; i < n; i++) {
-    for (let j = 2; j < i; j++) {
-        if (i % j ==0)
-         continue nextPrime;
+for (let i = 0; i < week.length; i++) {
+  if (i == today) {
+    if (week[i] === 'Сб' || week[i] === 'Вс') {
+      document.write(`<p><b><i>${week[i]}</i></b></p>`);
+    } else {
+      document.write(`<p><b>${week[i]}</b></p>`);
     }
-    console.log(i + ' Делители этого числа 1 и ' + i);
-};
+  } else if (week[i] === 'Сб' || week[i] === 'Вс') {
+    document.write(`<p><i>${week[i]}</i></p>`);
+  } else {
+    document.write(`<p>${week[i]}</p>`);
+  }
+}

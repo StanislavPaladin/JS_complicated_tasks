aa1e619961ebc4218437a5d9bff2920440f7a6d7
StanislavPaladin <PaladinStanis@gmail.com>
2020-08-28 16:34:28 +0300

    WIP: doing lesson_14

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
-let changeColorBtn = document.getElementById('change');
-let wrapper = document.querySelector('.wrapper');
-let h1 = document.querySelector('h1');
+'use strict';
 
 
-changeColorBtn.onclick = function() {
-    let symbols = '0123456789ABCDEF';
-    let color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += symbols[Math.floor(Math.random() * 16)];
-    }
-    h1.innerHTML = color;
-    wrapper.style.backgroundColor = color;
-    return color;
-  };
-  
-  
-  
-  function setRandomColor() {
-    $("#colorpad").css("background-color", getRandomColor());
-  }
+function DomElement(selector, height, width, bg, fontSize){
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+}
 
+DomElement.prototype.create = function(){
+    let newDiv = document.createElement('div');
+    console.log(Array.from(this.selector)[0]);
+    newDiv.innerHTML = 'Hello world for example';
 
+    newDiv.style.cssText=`height:${this.height};
+    width: ${this.width};
+     background: ${this.bg};
+    font-size: ${this.fontSize};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
 
+    if(Array.from(this.selector)[0] === '#'){
+    newDiv.id = this.selector;
+} else if (Array.from(this.selector)[0] === '.'){
+    newDiv.className = (this.selector);
+}
+    document.body.append(newDiv);
+    console.log(newDiv.style.cssText);
+};
+
+let newEl = new DomElement('.testid', '300px', '500px', 'green', '40px');
+console.log(newEl);
+
+newEl.create();
+
+// DomElement.prototype.createElement();

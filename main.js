374e3c6765bb5b4f2d9940791522b3ccfdcf3c3a
StanislavPaladin <PaladinStanis@gmail.com>
2020-09-16 15:35:39 +0300

    feat: completed lesson_14 task

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(event){
 'use strict';
-
+let newDiv = document.createElement('div');
 
 function DomElement(selector, height, width, bg, fontSize){
     this.selector = selector;
@@ -11,16 +12,12 @@ function DomElement(selector, height, width, bg, fontSize){
 
 DomElement.prototype.create = function(){
     let newDiv = document.createElement('div');
-    console.log(Array.from(this.selector)[0]);
-    newDiv.innerHTML = 'Hello world for example';
+    newDiv.innerHTML = '';
 
     newDiv.style.cssText=`height:${this.height};
     width: ${this.width};
      background: ${this.bg};
-    font-size: ${this.fontSize};
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    position: absolute;
   `;
 
     if(Array.from(this.selector)[0] === '#'){
@@ -29,12 +26,34 @@ DomElement.prototype.create = function(){
     newDiv.className = (this.selector);
 }
     document.body.append(newDiv);
-    console.log(newDiv.style.cssText);
+    
+    let count = 0;
+    let countTop = 0;
+    window.onkeydown = (e) => {
+       
+        if(e.keyCode === 39) {
+            count = count + 10;
+            newDiv.style.marginLeft = count + 'px';
+        } else if (e.keyCode === 37 && count >= 0) {
+            count = count - 10;
+            newDiv.style.marginLeft = count + 'px';
+        } else if (e.keyCode === 38  && countTop >= 0) {
+            countTop = countTop - 10;
+            newDiv.style.marginTop = countTop + 'px';
+        } else if (e.keyCode === 40) {
+            countTop = countTop + 10;
+            newDiv.style.marginTop = countTop + 'px';
+        }
+    
+    };
+   
+    
 };
 
-let newEl = new DomElement('.testid', '300px', '500px', 'green', '40px');
-console.log(newEl);
+let newEl = new DomElement('.testid', '100px', '100px', 'green', '40px');
 
-newEl.create();
 
-// DomElement.prototype.createElement();
+    newEl.create();
+
+
+});
